c4e8e6a533b98453bdb5e9ced28596a6aaeb9a0d
spacemonaut <4585677+spacemonaut@users.noreply.github.com>
2020-07-10 23:01:28 +0100

    randomly sized dungeon room

--- Dungeon.gd
+++ Dungeon.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+const WALLS = preload("res://Walls.tscn")
+
 func _ready():
+	randomize()
 	$Adventurer.position = $StartPosition.position
 	$Adventurer.target = $StartPosition.position + Vector2(32, 32)
+
+	var walls = WALLS.instance()
+	$Background.add_child(walls)
+	walls.rect_size.x = randi() % 400 + 200
+	walls.rect_size.y = randi() % 400 + 200
