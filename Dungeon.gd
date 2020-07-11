c3b6360dd7a4e1a19a51453a9689e6f8e4fd3e52
spacemonaut <4585677+spacemonaut@users.noreply.github.com>
2020-07-11 12:27:10 +0100

    basic main UI

--- Dungeon.gd
+++ Dungeon.gd
@@ -6,8 +6,3 @@ func _ready():
 	randomize()
 	$Adventurer.position = $StartPosition.position
 	$Adventurer.target = $StartPosition.position + Vector2(32, 32)
-
-	var walls = WALLS.instance()
-	$Background.add_child(walls)
-	walls.rect_size.x = randi() % 400 + 200
-	walls.rect_size.y = randi() % 400 + 200
