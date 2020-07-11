743be08c970dd451455f55e1a5488054c858cb0e
spacemonaut <4585677+spacemonaut@users.noreply.github.com>
2020-07-11 20:56:26 +0100

    enemy only wobbles when moving

--- characters/Enemy.gd
+++ characters/Enemy.gd
@@ -1,2 +1,16 @@
 extends Area2D
 
+export var target = Vector2(300, 100)
+export var movement_speed = 60
+
+func _process(delta):
+	var velocity = Vector2()
+
+	if position.distance_to(target) > 1:
+		velocity = target - position
+		velocity = velocity.normalized() * movement_speed
+
+	position += velocity * delta
+	
+	$Wobbler.on = velocity.length_squared() > 0
+	
--- tools/Wobbler.gd
+++ tools/Wobbler.gd
@@ -3,15 +3,21 @@ extends RemoteTransform2D
 enum Direction { LEFT = -1, RIGHT = 1 }
 
 var current_direction = Direction.RIGHT
-export var max_rotation_radians = 0.5
+export var max_rotation_radians = 0.3
 export var wobble_height_px = 4
 export var hertz = 2
+export var on = false
 
 func _process(delta):
+	if !on:
+		rotation = 0
+		current_direction = Direction.RIGHT
+		return
+	
 	var full_cycle = max_rotation_radians * 2
 	var wobble_speed = full_cycle * hertz * 2 * delta
 	rotation += wobble_speed * current_direction
-		
+	
 	if rotation > max_rotation_radians:
 		current_direction = Direction.LEFT
 	elif rotation < (-1 * max_rotation_radians):
